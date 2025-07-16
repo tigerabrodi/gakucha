f47909876f1001cc9bb9c38d2b11c047e6f4e088
Tiger Abrodi <tigerabrodi@gmail.com>
2025-07-16 08:53:32 +0200

    fix type imports

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,7 +1,7 @@
 import { Password } from '@convex-dev/auth/providers/Password'
 import { convexAuth } from '@convex-dev/auth/server'
-import { DataModel } from './_generated/dataModel'
-import { MutationCtx } from './_generated/server'
+import type { DataModel } from './_generated/dataModel'
+import type { MutationCtx } from './_generated/server'
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
   providers: [Password<DataModel>()],
